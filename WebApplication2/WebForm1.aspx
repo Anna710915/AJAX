19b31551581c5223d4b0c840ddc9e5a173d47a0a
Anna Merkul <anna.merkul@bk.ru>
2023-02-21 11:29:55 +0300

    AJAX

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,126 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<%--<head runat="server">
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+    <div>
+       <h4>Select a file to upload:</h4>
+   
+       <asp:FileUpload id="FileUpload1"                 
+           runat="server">
+       </asp:FileUpload>
+            
+       <br /><br />
+       
+       <asp:Button id="UploadButton" 
+           Text="Upload file"
+           OnClick="UploadButton_Click"
+           runat="server">
+       </asp:Button>    
+       
+       <hr />
+       
+       <asp:Label id="UploadStatusLabel"
+           runat="server">
+       </asp:Label> 
+        <br>
+        <asp:Image id="Image1" runat="server"
+           AlternateText="Image text"
+           ImageAlign="left"
+           ImageUrl="images/isewtweetbg.jpg" Height="500px" Width="600px"/>     
+        
+    </div>
+    </form>
+</body>
+</html>--%>
+
+
+<head runat="server">
+
+    <title>Untitled Page</title>
+
+</head>
+
+<body>
+
+    <form id="form1" runat="server">
+
+    <asp:ScriptManager ID="ScriptManager1" runat="server" />
+
+      
+
+    <div>
+
+      <h1>
+
+      Сколько стоит воздух?
+
+      </h1>
+
+   <br />
+
+  
+
+   Result:
+
+   <asp:UpdatePanel runat="server" ID="ResultPanel">
+
+   <ContentTemplate>
+
+   <br />
+   <asp:Label runat="server" ID="Label1">Евро : </asp:Label>
+   <asp:Label runat="server" ID="ResultrLabel"></asp:Label>
+       
+   <br />
+       <asp:Label runat="server" ID="Label2">Доллар США : </asp:Label>
+       <asp:Label runat="server" ID="Label3"></asp:Label>
+   <br />
+       <asp:Label runat="server" ID="Label4">Российский рубль : </asp:Label>
+       <asp:Label runat="server" ID="Label5"></asp:Label>
+       <asp:Timer ID="Timer1" runat="server"  Interval="1500" OnTick="Timer1_Tick">
+       </asp:Timer>
+   <br />
+   <small>
+
+   Panel render Time=<%=DateTime.Now.ToString() %></small>
+
+      </ContentTemplate>
+
+      
+
+   </asp:UpdatePanel>
+
+   
+
+   <asp:UpdateProgress runat="server" ID="UpdateProgress1">
+
+   <ProgressTemplate>
+
+   
+
+   </ProgressTemplate>
+
+   
+
+   </asp:UpdateProgress>
+
+   
+
+   <small>Page render Time2=<% =DateTime.Now.ToString() %></small>
+
+    
+
+    </div>
+
+    </form>
+
+</body>
+
+
+</html>
+
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+using System.Collections;
+
+using System.Configuration;
+
+using System.Data;
+
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+using System.Web.Security;
+
+using System.Web.UI;
+
+using System.Web.UI.HtmlControls;
+
+using System.Web.UI.WebControls;
+
+using System.Web.UI.WebControls.WebParts;
+
+using System.Xml.Linq;
+
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //TimerCallback tm = new TimerCallback(DoActioan);
+            //System.Threading.Timer timer = new System.Threading.Timer(tm, null, 0, 1500);
+            
+        }
+
+        private void Timer()
+        {
+            
+        }
+
+        private void DoActioan()
+        {
+            var random = new Random();
+
+            int rangeEuro = random.Next(0, 1000000);
+            double euro = random.NextDouble() * rangeEuro;
+
+            int rangeDollar = random.Next(0, 1000000);
+            double dollar = random.NextDouble() * rangeDollar;
+
+            int rangeRuble = random.Next(0, 1000000);
+            double ruble = random.NextDouble() * rangeRuble;
+
+            ResultrLabel.Text = euro.ToString();
+            Label3.Text = dollar.ToString();
+            Label5.Text = ruble.ToString();
+
+        }
+
+        protected void GoButton_Click(object sender, EventArgs e)
+
+        {
+
+          
+
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            DoActioan();
+        }
+
+
+        //protected void UploadButton_Click(object sender, EventArgs e)
+        //{
+        //// Specify the path on the server to
+        //// save the uploaded file to.
+        //String savePath = @"C:\Users\admin\source\ссп\ajax\SSP\WebApplication2\WebApplication2\images\";
+
+        //// Before attempting to perform operations
+        //// on the file, verify that the FileUpload 
+        //// control contains a file.
+        //if (FileUpload1.HasFile)
+        //{
+        //    // Get the name of the file to upload.
+        //    String fileName = FileUpload1.FileName;
+
+        //    // Append the name of the file to upload to the path.
+        //    savePath += fileName;
+
+
+        //    // Call the SaveAs method to save the 
+        //    // uploaded file to the specified path.
+        //    // This example does not perform all
+        //    // the necessary error checking.               
+        //    // If a file with the same name
+        //    // already exists in the specified path,  
+        //    // the uploaded file overwrites it.
+        //    FileUpload1.SaveAs(savePath);
+
+        //    // Notify the user of the name of the file
+        //    // was saved under.
+        //    UploadStatusLabel.Text = "Your file was saved as " + fileName;
+        //    Image1.ImageUrl = "images/"+fileName;
+        //}
+        //else
+        //{
+        //    // Notify the user that a file was not uploaded.
+        //    UploadStatusLabel.Text = "You did not specify a file to upload.";
+        //}
+
+        //}
+    }
+}
